4b05fef5053438726065c73cc9f37713d1dab783
makoto2025 <aratamakoto2025@gmail.com>
2026-01-27 12:34:11 -0800

    ex 4 solved

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,4 +1,5 @@
-/*In this exercise, we will be given a list of instructors and have to determine which instructor has the longest name.
+/*
+In this exercise, we will be given a list of instructors and have to determine which instructor has the longest name.
 
 Input
 
@@ -6,11 +7,20 @@ Expected Output
 
 
 Instruction
-Create a function named instructorWithLongestName that will receive an array of instructor objects, and return the object that has the longest name. If there are two instructors with the longest name, return the first one.
+? Create a function named instructorWithLongestName that will receive an array of instructor objects, 
+? and return the object that has the longest name. 
+? If there are two instructors with the longest name, return the first one.
 */
 
 const instructorWithLongestName = function (instructors) {
-  // Put your solution here
+  let longestElement = instructors[0];
+  for (let i = 1; i < instructors.length; i++) {
+    // console.log('i', i);
+    if ( instructors[i].name.length > longestElement.name.length ){
+      longestElement = instructors[i];
+    }
+  }
+  return longestElement;
 };
 
 console.log(
@@ -21,13 +31,22 @@ console.log(
     { name: "Donald", course: "Web" },
   ])
 ); //{name: "Jeremiah", course: "Web"}
-
 console.log(
   instructorWithLongestName([
-    { name: "Matthew", course: "Web" },
-    { name: "David", course: "Mobile" },
-    { name: "Domascus", course: "Web" },
+    { name: "111", course: "Mobile" },
+    { name: "222222", course: "Web" },
+    { name: "3333333", course: "Web" },
+    { name: "44444444", course: "Web" },
   ])
-); //{name: "Domascus", course: "Web"}
+); //{name: "Jeremiah", course: "Web"}
+
+// console.log(
+//   instructorWithLongestName([
+//     { name: "Matthew", course: "Web" },
+//     { name: "Davidsdajkfewijfi", course: "Mobile" },
+//     { name: "LONGGGGGGGgggggggggg", course: "Mobile" },
+//     { name: "Domascus", course: "Web" },
+//   ])
+// ); //{name: "Domascus", course: "Web"}
 
 module.exports = instructorWithLongestName;
