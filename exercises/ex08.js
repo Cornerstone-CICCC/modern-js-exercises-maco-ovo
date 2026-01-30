4ef0fae2ae084f49543c3f3aa80df77e238e4c19
makoto2025 <aratamakoto2025@gmail.com>
2026-01-30 12:29:36 -0800

    ex 8 solved

--- exercises/ex08.js
+++ exercises/ex08.js
@@ -1,18 +1,28 @@
 /*
-The input data for this exercise will be two dimensional array (an array of arrays), where each sub-array will have two numeric values. For example:
+The input data for this exercise will be two dimensional array (an array of arrays), 
+where each sub-array will have two numeric values. For example:
 
 [[1, 2], [2, 3]]
-The first will be the value to repeat, the second will be the amount of times to repeat that value.
+- The first will be the value to repeat, the second will be the amount of times to repeat that value.
 
-Instruction
-Create a function named repeatNumbers that will return a string with each of the given values repeated the appropriate number of times, if there are multiple sets of values each set should be separated by a comma. If there is only one set of values then you should omit the comma.
+> Instruction
+? Create a function named 'repeatNumbers' 
+? that will return a string with each of the given values repeated the appropriate number of times,
+? if there are multiple sets of values each set should be separated by a comma. 
+? If there is only one set of values then you should omit the comma.
 */
 
 const repeatNumbers = function (data) {
-  // Put your solution here
+let res = []
+  for (array of data ) { 
+  res.push(array[0].toString().repeat(array[1]))
+}
+return res.join(", ");
+
 };
 
-console.log(repeatNumbers([[1, 10]])); // 1111111111
+console.log(repeatNumbers([[1, 10]])); 
+// 1111111111
 console.log(
   repeatNumbers([
     [1, 2],
